c687979c2773aa251126e1385d114552db2ea62e
Mohammed Sadiq <sadiqmmm@gmail.com>
2019-03-11 01:37:37 +0530

    authgard not working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './public/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './public/register/register.module#RegisterPageModule' },
-  { path: 'dashboard', loadChildren: './members/dashboard/dashboard.module#DashboardPageModule' },
+  {
+    path: 'members',
+    canActivate: [AuthGuardService],
+    loadChildren: './members/member-routing.module#MemberRoutingModule'
+  }
 ];
 
 @NgModule({
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,15 @@
+import { AuthenticationService } from './authentication.service';
 import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) {}
+
+  CanActivate(): boolean {
+    return this.authService.isAuthenticated();
+  }
 }
