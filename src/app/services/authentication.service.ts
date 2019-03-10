b1f3738e55c442a5921e17616d583c87d045a35d
Mohammed Sadiq <sadiqmmm@gmail.com>
2019-03-10 23:28:28 +0530

    Add the method require for login

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Storage } from '@ionic/storage';
+import { Platform } from '@ionic/angular';
+
+const TOKEN_KEY = 'auth-token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  authenticateState = new BehaviorSubject(false);
 
-  constructor() { }
+  constructor(private storage: Storage, private plt: Platform) {
+    this.plt.ready().then(() => {
+      this.checkToken();
+    });
+  }
+
+  login() {
+    return this.storage.set(TOKEN_KEY, 'bearer 123456').then(res => {
+      this.authenticateState.next(true);
+    });
+  }
+
+  logout() {
+    return this.storage.remove(TOKEN_KEY).then(() => {
+      this.authenticateState.next(false);
+    });
+  }
+  isAuthenticated() {
+    return this.authenticateState.value;
+  }
+  checkToken() {
+    return this.storage.get(TOKEN_KEY).then(res => {
+      if (res) {
+        this.authenticateState.next(true);
+      }
+    });
+  }
 }
